433eed7acdb2930b638dca1c0b13ce4913e7e106
Ifycode <marychord@gmail.com>
2022-02-05 07:58:45 +0100

    Learning: Fetching data with useEffect

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import './TripList.css';
 
 export default function TripList() {
-    const [trips, setTrips] = useState([])
+    const [trips, setTrips] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/trips')
+            .then(response => response.json())
+            .then(json => setTrips(json));
+    }, []);
 
     //console.log(trips);
 
-    fetch('http://localhost:3000/trips')
-    .then(response => response.json())
-    .then(json => setTrips(json));
   return (
-      <div>
+      <div className="trip-list">
           <h2>Trip List</h2>
+          <ul>
+              {trips.map(trip => (
+                  <li key={trip.id}>
+                      <h3>{trip.title}</h3>
+                      <p>{trip.price}</p>
+                  </li>
+              ))}
+          </ul>
       </div>
   );
 }
