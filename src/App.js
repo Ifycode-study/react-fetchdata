886342d2139268d4e04e5d6ce9cb70147a40fa6a
Ifycode <marychord@gmail.com>
2022-02-05 17:12:51 +0100

    Learning: why we need a cleanup function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import TripList from './components/TripList';
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true);
+
   return (
     <div className="App">
-      <TripList />
+      <button onClick={() => setShowTrips(false)}>Hide trips</button>
+      {showTrips && <TripList />}
     </div>
   );
 }
